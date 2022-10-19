cb253a564602bcbd103e702c37cd64ee7da5ccbf
Michal Radomski <michalra@op.pl>
2022-10-19 15:31:21 +0200

    Express_101 - initial commit

---
+++ 02_Express_101/expressServer.ts
@@ -0,0 +1,26 @@
+import express, { Express, Request, Response } from "express";
+import path from "path";
+
+// Import routes
+
+// The server
+const app: Express = express();
+
+// Middlewares
+
+//Route middleware
+
+// Test route
+app.get("/", (req: Request, res: Response) => {
+  console.log("req.ip:", req.ip);
+  res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
+});
+
+// Port
+const port = (process.env.PORT || 5000) as number;
+
+app.listen({ port: port }, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+  // For testing only
+  console.log("Current Time:", new Date().toLocaleTimeString());
+});
