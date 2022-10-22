d43006c67e90a9886004cab66a965c0b0a238c9f
Michal Radomski <michalra@op.pl>
2022-10-22 19:33:36 +0200

    MovieApp - initial commit

---
+++ 05_MovieApp/server.ts
@@ -0,0 +1,42 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+// import path from "path";
+
+// Import routes
+
+// The server
+const app: Express = express();
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("combined"));
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginResourcePolicy: false,
+    crossOriginEmbedderPolicy: false,
+    crossOriginOpenerPolicy: false,
+  })
+);
+
+//Route middleware
+
+// Test route
+app.get("/", (req: Request, res: Response) => {
+  console.log("req.ip:", req.ip);
+  res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
+});
+
+// Port
+const port = (process.env.PORT || 5000) as number;
+
+app.listen({ port: port }, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+  // For testing only
+  console.log("Current Time:", new Date().toLocaleTimeString());
+});
