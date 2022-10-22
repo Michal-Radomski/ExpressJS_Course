a2eb105e573d7cbff246de1df59d1d1493555064
Michal Radomski <michalra@op.pl>
2022-10-22 22:41:38 +0200

    MovieApp - commit1

--- 05_MovieApp/routes/index.ts
+++ 05_MovieApp/routes/index.ts
@@ -4,7 +4,8 @@ const indexRouter: Router = express.Router();
 
 indexRouter.get("/", (req: Request, res: Response) => {
   console.log("req.ip:", req.ip);
-  res.send("<h1 style='color:green;text-align:center'>API is running - indexRouter.ts</h1>");
+  // res.send("<h1 style='color:green;text-align:center'>API is running - indexRouter.ts</h1>");
+  res.render("index", {});
 });
 
 export default indexRouter;
--- 05_MovieApp/server.ts
+++ 05_MovieApp/server.ts
@@ -16,12 +16,12 @@ import indexRouter from "./routes";
 const app: Express = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Middlewares
 app.use(cookieParser());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("combined"));
