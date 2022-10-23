f58022c453bd1c053e624e8ac4247fb2ad985742
Michal Radomski <michalra@op.pl>
2022-10-23 21:20:05 +0200

    Api - initial commit

---
+++ 06_Api/server.ts
@@ -0,0 +1,64 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express, { Express, NextFunction, Request, Response } from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+import createError from "http-errors";
+import path from "path";
+import http from "http";
+
+// Import routes
+import indexRouter from "./routes";
+
+// The server
+const app: Express = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Middlewares
+app.use(cookieParser());
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("combined"));
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginResourcePolicy: false,
+    crossOriginEmbedderPolicy: false,
+    crossOriginOpenerPolicy: false,
+  })
+);
+
+//* Favicon
+app.get("/favicon.ico", (_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname + "/public/favicon.png"));
+});
+
+// Test route
+app.get("/test", (req: Request, res: Response) => {
+  console.log("req.ip:", req.ip);
+  res.send("<h1 style='color:blue;text-align:center'>API is running - server.ts</h1>");
+});
+
+//Route middleware
+app.use("/", indexRouter);
+
+const errorPage = createError(404, "This page does not exist!");
+app.use(function (_req: Request, _res: Response, next: NextFunction) {
+  next(errorPage);
+});
+
+// Port
+const port = (process.env.PORT || 5000) as number;
+
+const server = http.createServer(app);
+server.listen({ port: port }, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+  // For testing only
+  console.log("Current Time:", new Date().toLocaleTimeString());
+});
