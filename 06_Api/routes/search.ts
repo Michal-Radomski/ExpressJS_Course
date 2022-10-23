0137cab1a4b8a2de0ccf98819fc68fb9032a9fe9
Michal Radomski <michalra@op.pl>
2022-10-24 01:39:55 +0200

    Api - commit7 - Movie Api done

--- 06_Api/routes/search.ts
+++ 06_Api/routes/search.ts
@@ -1,10 +1,50 @@
-import express, { Request, Response, Router } from "express";
+import express, { NextFunction, Request, Response, Router } from "express";
+
+import movies from "../data/movies";
+import people from "../data/people";
 
 const searchRouter: Router = express.Router();
 
 searchRouter.get("/", (req: Request, res: Response) => {
   console.log("req.ip:", req.ip);
-  res.send("<h1 style='color:orangered;text-align:center'>API is running - searchRouter.ts</h1>");
+  // res.send("<h1 style='color:orangered;text-align:center'>API is running - searchRouter.ts</h1>");
+  res.render("index", { title: "Express Movie Api" });
+});
+
+function queryRequired(req: Request, res: Response, next: NextFunction) {
+  const searchTerm = req.query.query as string;
+  if (!searchTerm) {
+    res.json({ msg: "Query is required." });
+  } else {
+    next();
+  }
+}
+
+// This middleware will be used by ALL routes in THIS router
+searchRouter.use(queryRequired);
+
+// GET /search/movie
+searchRouter.get("/movie", (req: Request, res: Response) => {
+  const searchTerm = req.query.query as string;
+  // console.log("req.query:", req.query);
+  // console.log({searchTerm})
+  const results = movies.filter((movie) => {
+    const found = movie.overview.includes(searchTerm) || movie.title.includes(searchTerm);
+    return found;
+  });
+  res.json({ results: results });
+});
+
+// GET /search/person
+searchRouter.get("/person", (req: Request, res: Response) => {
+  const searchTerm = req.query.query as string;
+  // console.log("req.query:", req.query);
+  // console.log({searchTerm})
+  const results = people.filter((person) => {
+    const found = person.name.includes(searchTerm);
+    return found;
+  });
+  res.json({ results: results });
 });
 
 export default searchRouter;
