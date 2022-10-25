345833da3fa32c06a2d13a9a34e0bc0aa0711f84
Michal Radomski <michalra@op.pl>
2022-10-25 14:47:27 +0200

    Multer - initial commit

---
+++ 09_Multer/routes/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response, Router } from "express";
+
+const indexRouter: Router = express.Router();
+
+indexRouter.get("/", (req: Request, res: Response) => {
+  console.log("req.ip:", req.ip);
+  // res.send("<h1 style='color:green;text-align:center'>API is running - indexRouter.ts</h1>");
+  res.render("index", { title: "Express Multer" });
+});
+
+export default indexRouter;
